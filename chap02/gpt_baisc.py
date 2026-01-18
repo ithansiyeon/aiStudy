a19d557d4e181fe11896815f0f3da592df579a0f
itsiyeon <itsiyeon@nate.com>
2026-01-18 15:40:21 +0900

    initial commit

---
+++ chap02/gpt_baisc.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=api_key)
+
+response = client.chat.completions.create(
+    model = "gpt-4o",
+    temperature = 0.1,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "system", "content": "2022년 월드컵 우승 팀은 어디야?"},
+    ]
+)
+
+print(response)
+
+print('-----')
+print(response.choices[0].message.content)
